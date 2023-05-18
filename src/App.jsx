2b1de9a879ac4e5809ec0959da892067a2d9eb6f
KAMBONO <kambonoj@gmail.com>
2023-05-19 02:02:54 +0300

    initial commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './index.css'
 import Navbar from './components/navbar/Navbar'
+import Home from './components/home/Home'
+import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
+
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
+      <Main/>
+      <Footer/>
+
     </div>
   )
 }
